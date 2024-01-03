6979905a6bea4238f0076fcc19492c635290872f
Fahim-Khan-98 <fahim.khancsebd@gmail.com>
2024-01-03 11:37:30 +0600

    [Add] cell

---
+++ iterate_numpy_array_using_nditer.py
@@ -0,0 +1,21 @@
+# # Example 1
+
+# import numpy as np
+
+# a = np.arange(15).reshape( 5,3)
+
+# print("array : ",a)
+# print("===")
+# aw=0
+
+# for row in a:
+#   print(row)
+# print("===")
+# for row in a:
+#   for cell in row:
+#     print("Element:", cell)
+#     aw += cell
+# print("===")
+# print("Cumulative Sum:", aw)
+
+
